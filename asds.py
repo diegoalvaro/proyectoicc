bb511b21422de70b298b30ccab8b3ded8166d15d
diegoalvaro <40146307+diegoalvaro@users.noreply.github.com>
2018-06-10 23:46:15 -0500

    Add files via upload

---
+++ asds.py
@@ -0,0 +1,11 @@
+num =int(input('digite numero:'))
+factorial=1
+
+
+def factorial(num):
+    for i in range(num):
+        factorial=factorial*num
+        num=num-1
+
+re=factorial(num)
+print(re)
